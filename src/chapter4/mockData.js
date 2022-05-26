62893f0c2a9256d01e22aa4aaa7f2a04f20b32ba
문주영 <felixmoon@cenacle.com>
2022-05-26 10:38:35 +0900

    first test

--- src/chapter4/mockData.js
+++ src/chapter4/mockData.js
@@ -3,7 +3,9 @@ function sampleProvinceData() {
         name: "Asia",
         producers: [
             {name: "Byzantium", cost: 10, production: 9},
-        ]
+        ],
+        demand: 30,
+        price: 20
     }
 }
 
--- test/test.province.js
+++ test/test.province.js
@@ -1,7 +1,8 @@
-import Province from '../src/Province';
+import Province from '../src/chapter4/Province.js';
 
 describe('province', function() {
     it('shortfall', function() {
         const asia = new Province(sampleProvinceData());
+        assert.equal(asia.shortfall, 5)
     })
 })
