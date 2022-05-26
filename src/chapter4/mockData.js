e67acb6749748d1e6789193e50e66fb49039e02a
문주영 <felixmoon@cenacle.com>
2022-05-26 10:12:45 +0900

    chapter4-example

---
+++ src/chapter4/mockData.js
@@ -0,0 +1,12 @@
+function sampleProvinceData() {
+    return {
+        name: "Asia",
+        producers: [
+            {name: "Byzantium", cost: 10, production: 9},
+        ]
+    }
+}
+
+export {
+    sampleProvinceData
+}
---
+++ test/test.province.js
@@ -0,0 +1,7 @@
+import Province from '../src/Province';
+
+describe('province', function() {
+    it('shortfall', function() {
+        const asia = new Province(sampleProvinceData());
+    })
+})
